492bccea7b1c7b62c0608e38db2d1be003ebca7b
bhargav23 <bhargav.kiran@gmail.com>
2023-12-14 00:14:13 +0530

    Create i.c

---
+++ week9/i.c
@@ -0,0 +1,13 @@
+//Write a C program to find the sum of a 1D array using malloc()
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int *p,n,i;
+	scanf("%d",&n);
+	p=(int *)malloc(sizeof(int*));
+	for(i=0;i<n;i++)
+		scanf("%d",(p+i));
+	for(i=0;i<n;i++)
+		printf("%d ",*(p+i));
+}
