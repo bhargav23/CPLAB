f28028226cef7094a51bc73555fdc00981a93b67
bhargav23 <bhargav.kiran@gmail.com>
2023-12-14 21:26:04 +0530

    Create 4.c

---
+++ week9/4.c
@@ -0,0 +1,38 @@
+// ii) Write a C program to find the total, average of n students using structures
+#include<stdio.h>
+	struct student{
+		char name[50];
+		int rno;
+		float marks[3];
+		float total;
+	};
+int main()
+{
+	int i,j,n,sum=0;
+	float avg;
+	struct student s[10];
+	printf("Enter the no of students");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		printf("Enter the name,rno and marks of the student %d : ",i+1);
+		scanf("%s",s[i].name);
+		scanf("%d",&s[i].rno);
+		for(j=0;j<3;j++) {
+			printf("Enter Marks of Subject %d : ",i+1);
+			scanf("%f",&s[i].marks[j]);
+		}
+			
+	}
+	for(i=0;i<n;i++){
+		sum = 0;
+		for(j=0;j<3;j++) {
+			sum = sum + s[i].marks[j]; //Sum of all the subjects
+		}
+		s[i].total = sum;
+	}	
+	for(i=0;i<n;i++){
+		printf("Name = %s\n",s[i].name);
+		printf("RNo = %d\n",s[i].rno);
+		printf("Total Marks = %f\n",s[i].total);
+	}
+}
